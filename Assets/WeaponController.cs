9b0b204d0e6a5344b0b404408f894c20147a4402
bugra <bugra.omursun@gmail.com>
2022-04-13 23:55:50 +0300

    Attack animation, animation blend tree improvements

--- Assets/WeaponController.cs
+++ Assets/WeaponController.cs
@@ -10,7 +10,7 @@ public class WeaponController : MonoBehaviour
 	[SerializeField] private Transform weapon;
 	[SerializeField] private Vector3 handAttachPos;
 	[SerializeField] private Vector3 handAttachRot;
-	
+
 	[SerializeField] private Transform sheath;
 	[SerializeField] private Vector3 sheathAttachPos;
 	[SerializeField] private Vector3 sheathAttachRot;
@@ -19,8 +19,22 @@ public class WeaponController : MonoBehaviour
 	public void OnDrawWeapon()
 	{
 		isDraw = !isDraw;
-		this.GetComponent<Animator>().SetBool("DrawSword",isDraw);
-		Debug.Log("Test");
+		if (isDraw)
+		{
+			this.GetComponent<Animator>().SetTrigger("DrawSword");
+		}
+		else
+		{
+			this.GetComponent<Animator>().SetTrigger("SheatSword");
+		}
+		this.GetComponent<Animator>().SetBool("isWeaponDraw", isDraw);
+	}
+	public void OnAttack()
+	{
+		if (isDraw == true)
+		{
+			this.GetComponent<Animator>().SetTrigger("Attack");
+		}
 	}
 	public void AttachWeaponToHand()
 	{
@@ -28,7 +42,7 @@ public class WeaponController : MonoBehaviour
 		weapon.transform.localPosition = handAttachPos;
 		weapon.transform.localRotation = Quaternion.Euler(handAttachRot);
 	}
-	
+
 	public void AttackWeaponToSheath()
 	{
 		weapon.parent = sheath;

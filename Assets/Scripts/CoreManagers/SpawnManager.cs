d7b8d7ed143e2c816a0783604f1398750bf2de0e
Bugra Omursun <bugra.omursun@gmail.com>
2022-09-01 10:18:54 +0300

    random spawn added

--- Assets/Scripts/CoreManagers/SpawnManager.cs
+++ Assets/Scripts/CoreManagers/SpawnManager.cs
@@ -4,30 +4,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Sirenix.OdinInspector;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
 	[SerializeField] private GameTypes gameType;
-	public GameObject playerPrefab;
+	public GameObject[] playerPrefabs;
 
 	void Start()
 	{
 		Spawn();
 	}
-	
+
 	[Button]
 	public void Spawn()
 	{
 		//for single, multi selection
 		GameObject player;
-
+		GameObject spawnPlayer = playerPrefabs[Random.Range(0, playerPrefabs.Length)];
 		switch (gameType)
 		{
 			case GameTypes.SinglePlayer:
-				player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+				player = Instantiate(spawnPlayer, Vector3.zero, Quaternion.identity);
 				break;
 			case GameTypes.MultiPlayer:
-				player = PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
+				player = PhotonNetwork.Instantiate(spawnPlayer.name, Vector3.zero, Quaternion.identity);
 				break;
 		}
 	}

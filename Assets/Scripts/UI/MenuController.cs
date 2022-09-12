47a5f99c899e077b00c265473d178548b41bbb60
Bugra Omursun <bugra.omursun@gmail.com>
2022-09-12 14:28:49 +0300

    main menu improvements

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
 	[SerializeField] private Animator animator;
 	public void ChangeAnimatorState(int value)
 	{
 		animator.SetInteger("State", value);
+	}
+	public void SinglePlayer()
+	{
+		SceneManager.LoadScene("BattleArea");
+	}
+	public void ExitGame()
+	{
+		Application.Quit();
 	}
 }
